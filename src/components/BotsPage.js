c6045d0e5fd7748844085b647cfb44e225fb8f24
Kennedy Musau <kennedy.musauu@gmail.com>
2023-05-27 17:51:08 +0300

    added bots to collection

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const [bots, setBots] = useState([]);
+  const [myBotArmy, setMyBotArmy] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((res) => res.json())
+      .then((data) => setBots(data));
+  }, []);
+
+  function addBotToMyArmyHandler(){
+    console.log('Added to my army');
+  }
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection bots={bots} addBotToMyArmy={addBotToMyArmyHandler}/>
     </div>
-  )
+  );
 }
 
 export default BotsPage;
